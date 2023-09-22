0943583171ed832b8fe59bca46f46d393d39c34d
Twin J <junohanyere@gmail.com>
2023-09-22 16:54:30 +0100

    added bubble_sort and swap prototypes

--- sort.h
+++ sort.h
@@ -18,5 +18,7 @@ typedef struct listint_s
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void swap(int *a, int *b);
+void bubble_sort(int *array, size_t size);
 
 #endif	/* SORT_H */
