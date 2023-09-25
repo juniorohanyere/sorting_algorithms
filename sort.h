4fd225865cc7f3901c00765620201c39a0ec0c37
Twin J <junohanyere@gmail.com>
2023-09-25 02:11:23 +0100

    modified

--- sort.h
+++ sort.h
@@ -22,5 +22,6 @@ void swap(int *a, int *b);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void swap_nodes(listint_t **list, listint_t **node_a, listint_t *node_b);
+void selection_sort(int *array, size_t size);
 
 #endif	/* SORT_H */
