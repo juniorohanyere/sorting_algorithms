780b849194559ec0b306935beedc8087e35aeb02
Twin J <junohanyere@gmail.com>
2023-09-25 02:19:34 +0100

    resolved betty issue

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -20,6 +20,16 @@ void swap(int *a, int *b)
 	*b = tmp;
 }
 
+/**
+ * selection_sort - sorts an array of integers in ascending order using the
+ *		    Selection sort algorithm
+ *
+ * @array: the array of integers to sort
+ * @size: the size of @array
+ *
+ * Return: return nothing
+*/
+
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min;
